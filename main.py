d6518358e1745d9c6a0569ea86ea3c7add314ef3
hongzicong <973138379@qq.com>
2019-05-05 18:35:51 +0800

    update

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt;
-import seaborn as sns
-
-train, test = pd.read_csv('./data/train.csv'), pd.read_csv('./data/test.csv')
-
-sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
