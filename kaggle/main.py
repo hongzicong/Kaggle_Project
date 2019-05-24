92bfda689a1c572fb019f95e572c8d2a8834a951
hongzicong <973138379@qq.com>
2019-05-24 15:31:38 +0800

    update

--- kaggle/main.py
+++ kaggle/main.py
@@ -12,8 +12,12 @@ from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation, metrics
 
-data_train = pd.read_csv("C:/Users/Dv00/Desktop/new1datamining2019spring/trainSet.csv")
-data_test = pd.read_csv("C:/Users/Dv00/Desktop/new1datamining2019spring/test set.csv")
+# data_train = pd.read_csv("C:/Users/Dv00/Desktop/new1datamining2019spring/trainSet.csv")
+# data_test = pd.read_csv("C:/Users/Dv00/Desktop/new1datamining2019spring/test set.csv")
+
+data_train = pd.read_csv("../../new1datamining2019spring/trainSet.csv")
+data_test = pd.read_csv("../../new1datamining2019spring/test set.csv")
+
 
 x = data_train.iloc[:, 0:-1]
 y = data_train.iloc[:, -1]
@@ -23,18 +27,18 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
 # 使用随机森林
 
-rfc = RandomForestClassifier(n_estimators=1000, oob_score = True)
+rfc = RandomForestClassifier(n_estimators=50, oob_score = True)
 
 rfc.fit(x_train, y_train)
 
 rfc_y_predict = rfc.predict(x_test)
 
 #获取特征的重要性
-importances = rfc.feature_importances_
-indices = np.argsort(importances)[::-1]
-cols_name = data_train.columns[:-1]
-for f in range(x_train.shape[1]):
-    print("%2d) %-*s %f" % (f + 1,30,cols_name[indices[f]],importances[indices[f]]))
+#importances = rfc.feature_importances_
+#indices = np.argsort(importances)[::-1]
+#cols_name = data_train.columns[:-1]
+#for f in range(x_train.shape[1]):
+#    print("%2d) %-*s %f" % (f + 1,30,cols_name[indices[f]],importances[indices[f]]))
 
 print(rfc.score(x_test, y_test))
  
--- kaggle/test.py
+++ kaggle/test.py
@@ -5,24 +5,67 @@ Created on Tue Mar 26 21:47:23 2019
 @author: Dv00
 """
 
+
 import pandas as pd
-import sklearn
-from sklearn.cross_validation import train_test_split
+import numpy as np
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
 
-from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
 
-data_train = pd.read_csv("C:/Users/Dv00/Desktop/new1datamining2019spring/trainSet.csv")
-data_test = pd.read_csv("C:/Users/Dv00/Desktop/new1datamining2019spring/test set.csv")
+data_train = pd.read_csv("../../new1datamining2019spring/trainSet.csv")
+data_test = pd.read_csv("../../new1datamining2019spring/test set.csv")
+
 
 x = data_train.iloc[:, 0:-1]
 y = data_train.iloc[:, -1]
 
-param_test2 = {'min_samples_split':list(range(2,102,10))}
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
-gsearch2 = GridSearchCV(estimator = RandomForestClassifier(n_estimators=50, oob_score = True),
-   param_grid = param_test2, scoring='roc_auc', iid=False, cv=5)
+# Set the parameters by cross-validation
+parameter_space = {
+    "n_estimators": [10, 15, 20],
+    "criterion": ["gini", "entropy"],
+    "min_samples_leaf": [2, 4, 6],
+}
  
-gsearch2.fit(x, y)
-print(gsearch2.grid_scores_, gsearch2.best_params_, gsearch2.best_score_)
+#scores = ['precision', 'recall', 'roc_auc']
+scores = ['roc_auc']
+ 
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+ 
+    clf = RandomForestClassifier(random_state=14)
+    grid = GridSearchCV(clf, parameter_space, cv=5, scoring='%s' % score)
+    #scoring='%s_macro' % score：precision_macro、recall_macro是用于multiclass/multilabel任务的
+    grid.fit(x_train, y_train)
+ 
+    print("Best parameters set found on development set:")
+    print()
+    print(grid.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = grid.cv_results_['mean_test_score']
+    stds = grid.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, grid.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    bclf = grid.best_estimator_
+    bclf.fit(x_train, y_train)
+    y_true = y_test
+    y_pred = bclf.predict(x_test)
+    y_pred_pro = bclf.predict_proba(x_test)
+    y_scores = pd.DataFrame(y_pred_pro, columns=bclf.classes_.tolist())[1].values
+    print(classification_report(y_true, y_pred))
+    auc_value = roc_auc_score(y_true, y_scores)
